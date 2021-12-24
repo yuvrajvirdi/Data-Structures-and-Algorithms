1f923655ea8275b2065622cb33bfd4571a13e500
Yuvraj Virdi <81879713+yuvrajvirdi@users.noreply.github.com>
2021-12-23 22:29:06 -0500

    Update binaryTree.py

--- binaryTree.py
+++ binaryTree.py
@@ -3,3 +3,16 @@ class TreeNode:
         self.key = key
         self.left = None
         self.right = None
+
+node0 = TreeNode(3)
+node1 = TreeNode(4)
+node2 = TreeNode(5)
+
+node0.left = node1
+node0.right = node2
+
+tree = node0 
+
+tree.key # gives value of 3
+tree.left.key # gives value of 4
+tree.right.key # gives value of 5
