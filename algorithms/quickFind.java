eb80af0761db0eb347c3a4274981bc973f32b1aa
Yuvraj Virdi <81879713+yuvrajvirdi@users.noreply.github.com>
2022-01-08 22:05:06 -0500

    Add files via upload

---
+++ algorithms/quickFind.java
@@ -0,0 +1,21 @@
+public class quickFind {
+    private int[] id;
+
+    public quickFind(int N){
+        id = new int[N];
+        for (int i = 0; i < N; i++)
+        id[i] = i;
+    }
+
+    public boolean connected(int p, int q) {
+        return id[p]==id[q];
+    }
+
+    public void union(int p, int q){
+        int p_id = id[p];
+        int q_id = id[q];
+        for (int i = 0; i < id.length; i++)
+            if (id[i] == p_id)
+                id[i] = q_id;
+    }
+}
