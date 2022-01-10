cb0a4a1824116876e445af0693e83d940d912cea
Yuvraj Virdi <81879713+yuvrajvirdi@users.noreply.github.com>
2022-01-09 20:19:14 -0500

    Update LinkedQueue.java

--- algorithms/LinkedQueue.java
+++ algorithms/LinkedQueue.java
@@ -19,7 +19,7 @@ public class LinkedQueue {
     }
    public String dequeue(){
         String item = first.item;
-        first       = first.next;
+        first = first.next;
         if (isEmpty()) last = null;
         return item;
     }
