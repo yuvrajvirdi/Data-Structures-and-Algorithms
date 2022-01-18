90b9496fdac5caee99b2189ccb0c3d54a6d11fd8
Yuvraj Virdi <81879713+yuvrajvirdi@users.noreply.github.com>
2022-01-17 20:55:42 -0500

    Add files via upload

---
+++ hash maps/hash_map.py
@@ -0,0 +1,30 @@
+class HashTable(object):
+    def __init__(self):
+        self.table = [None]*10000
+
+    def store(self, string):
+        """Input a string that's stored in 
+        the table."""
+        hv = self.calculate_hash_value(string)
+        if hv != -1:
+            if self.table[hv] != None:
+                self.table[hv].append(string)
+            else:
+                self.table[hv] = [string]
+
+    def lookup(self, string):
+        """Return the hash value if the
+        string is already in the table.
+        Return -1 otherwise."""
+        hv = self.calculate_hash_value(string)
+        if hv != -1:
+            if self.table[hv] != None:
+                if string in self.table[hv]:
+                    return hv
+        return -1
+
+    def calculate_hash_value(self, string):
+        """Helper function to calulate a
+        hash value from a string."""
+        value = ord(string[0])*100 + ord(string[1])
+        return value
